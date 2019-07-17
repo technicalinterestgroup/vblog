195f37916b44bfae5100de7ec3d14de903f9f6b9
shuyu.wang <wangsy@ganinfo.com>
2019-07-17 23:00:51 +0800

    统一异常拦截

--- vblog-web/src/main/java/com/technicalinterest/group/config/MyWebAppConfig.java
+++ vblog-web/src/main/java/com/technicalinterest/group/config/MyWebAppConfig.java
@@ -19,10 +19,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class MyWebAppConfig extends WebMvcConfigurerAdapter {
 
-//    @Bean
-//    MyInterceptor myInterceptor() {
-//        return new MyInterceptor();
-//    }
+    @Bean
+    MyInterceptor myInterceptor() {
+        return new MyInterceptor();
+    }
 
 //	@Bean
 //	public HandlerInterceptor getRequestLimitInterceptor(){
