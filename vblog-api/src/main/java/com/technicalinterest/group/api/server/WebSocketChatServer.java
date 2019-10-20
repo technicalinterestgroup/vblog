42ebb586344f41ae418ac39666ef961b664a633c
wangshuyu <shuyu.wang01@mljr.com>
2019-10-20 13:24:17 +0800

    service(all):整合websocket提供消息推送和在线聊天功能

--- vblog-api/src/main/java/com/technicalinterest/group/api/server/WebSocketChatServer.java
+++ vblog-api/src/main/java/com/technicalinterest/group/api/server/WebSocketChatServer.java
@@ -1,6 +1,5 @@
 package com.technicalinterest.group.api.server;
 
-import com.alibaba.fastjson.JSONObject;
 import com.technicalinterest.group.service.util.WebSocketUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
